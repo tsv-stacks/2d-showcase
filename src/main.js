b5d8e749b3f8a1b3d9cd30dc734aa23e14a8fc74
tsv-stacks <tsvirdi@yahoo.co.uk>
2024-04-13 23:04:43 +0100

    add css and logic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 
 k.loadSprite("spritesheet", "./character-spritesheet.png", {
   sliceX: 24,
-  SliceY: 3,
+  sliceY: 3,
   anims: {
     "idle-down": { from: 42, to: 47, loop: true, speed: 8 },
     "walk-down": { from: 66, to: 71, loop: true, speed: 8 },
@@ -12,3 +13,54 @@ k.loadSprite("spritesheet", "./character-spritesheet.png", {
     "walk-up": { from: 54, to: 59, loop: true, speed: 8 },
   },
 });
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers = mapData.layers;
+
+  const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({ shape: new k.Rect(k.vec2(0, 3), 32, 32) }),
+    k.body(),
+    k.anchor("center"),
+    k.pos(),
+    k,
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: "down",
+      isInDialogue: false,
+    },
+    "player",
+  ]);
+
+  for (const layer of layers) {
+    if (layer.name === "boundaries") {
+      for (const boundary of layer.objects) {
+        map.add([
+          k.area({
+            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+          }),
+          k.body({ isStatic: true }),
+          k.pos(boundary.x, boundary.y),
+          boundary.name,
+        ]);
+
+        if (boundary.name) {
+          player.onCollide(boundary.name, () => {
+            player.isInDialogue = true;
+            // todo
+          });
+        }
+      }
+    }
+  }
+});
+
+k.go("main");
